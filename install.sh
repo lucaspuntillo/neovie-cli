519946d1bdbfe27610813ca5f08f402bf5fa1a8d
Lucas Puntillo <62703475+puntillol59@users.noreply.github.com>
2020-09-25 12:28:57 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+echo "Run this as root or use the sudo command."
+sleep 50
+sudo apt-get install unzip
+mkdir /usr/github-cli
+cd /usr/github-cli
+wget https://storage.live.com/downloadfiles/V1/Zip?application=1141147648
+progress-bar 15
+unzip -q gh_1.0.0_linux_amd64.zip
+rm gh_1.0.0_linux_amd64.zip
+cd $HOME
+echo 'gh=/usr/github-cli/gh_1.0.0_linux_amd64/gh_1.0.0_linux_amd64/bin/gh' >> .bashrc
+echo "Github Cli was intalled in /usr/github-cli/gh_1.0.0_linux_amd64/gh_1.0.0_linux_amd64/ type gh to use it."
