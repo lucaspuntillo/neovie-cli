4c9b05c1b12097a1ebc11bb72a589392f064b778
Lucas Puntillo <62703475+puntillol59@users.noreply.github.com>
2020-10-13 13:29:56 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ sudo apt-get install unzip
 mkdir /usr/github-cli
 cd /usr/github-cli
 wget https://storage.live.com/downloadfiles/V1/Zip?application=1141147648
-progress-bar 15
+bash progress-bar 15
 unzip -q gh_1.0.0_linux_amd64.zip
 rm gh_1.0.0_linux_amd64.zip
 cd $HOME
